49ebb5240a8cb26bb24a063d678f9477f35de6c4
Valentina <Valentina_B@mail.ru>
2022-05-13 15:25:46 +0300

    Задача 41

---
+++ Zadacha43/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Решение:
+y = k1 * x + b1 
+y = k2 * x + b2;
+
+k1 * x + b1 = k2 * x + b2;
+k1 * x - k2 * x = b2 - b1;
+x * (k1 - k2) = b2 - b1;
+x = (b2 - b1)/ (k1 - k2);
+y = k1 * ((b2 - b1)/ (k1 - k2)) + b1;*/ 
+
+
+
+double InputNumber(string data)
+{
+    Console.Write($"Введите значение {data} ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+double b1 = InputNumber("b1");
+double k1 = InputNumber("k1");
+double b2 = InputNumber("b2");
+double k2 = InputNumber("k2");
+
+double x = 0;
+double y = 0;
+
+x = (b2 - b1)/ (k1 - k2);
+y = k1 * x + b1;
+Console.WriteLine($"Координаты точки перечения 2-ух прямых ({x}; {y})");
+
+
+
+
+
---
+++ Zadacha_41/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3*/
+
+Console.WriteLine("Введите числа через запятую(после запятой пробел): ");
+string numbers = Console.ReadLine();
+
+int countNumbers = 1;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] == ',')
+    countNumbers++;
+}
+
+int[] array = new int[countNumbers];
+int arrayIndex = 0;
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    string subString = String.Empty;
+    while (numbers[i] != ',')
+    {
+        subString += numbers[i].ToString();
+        i++;
+
+        if (i >= numbers.Length)
+        break;
+    }
+
+    if(subString == "" || subString == " ")
+        continue;
+
+    array[arrayIndex] = Convert.ToInt32(subString);
+    arrayIndex++;
+}
+
+/*for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}                                          - ВЫВОД МАССИВА
+код ввода чисел и вывода их в массив,
+код ниже - проверка на кол-во чисел больше 0*/
+
+int count =0;
+for (int i = 0; i < array.Length; i++)
+if (array[i] > 0)
+count++;
+Console.Write($"Больше 0: {count}");
+
+
